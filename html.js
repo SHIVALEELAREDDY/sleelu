2a8e6f3c808dfcffaf38a445e6cd203d65d9834d
SHIVALEELAREDDY <itsshivureddy956@gmail.com>
2024-12-12 12:05:58 +0530

    files

--- html.js
+++ html.js
@@ -10,17 +10,47 @@
 // console.log(a+b+c);
 // }
 
-let a=+prompt("enter digit here");
+// prime number
+// let a=+prompt("enter a digit")
+// let count=0
+// if(a>=1){
+//     for(i=1;i<=a;i++){
+//         if(a%i==0){
+//             count++
+//         } 
+//         // console.log(a,);
+        
+//     }
+    
+//     if(count==2){
+//         console.log(a,"is a prime number")
+//     }else{
+//         console.log(a,"is not a prime number")
+//     }    
+//  }
+// //  else{
+// //     console.log(a,"is a prime number");
+    
+// }
+
+let a=+prompt("enter a digit");
 let count=0;
-for(i=1;i<=a;i++){
-    if(a % i==0){
-        count++
+
+if(a>=1){
+        console.log(a,"is lessthan 1 or equal to 1"); 
+}else{
+    for(i=1;i<=a;i++){
+        if(a%i==0){
+            count++
+        }
     }
 }
 if(count==2){
-    console.log(a,"is prime");  
+    console.log(a,"is a prime number");
+    
 }else{
-  console.log(a,"is not prime");
-  
+    console.log(a,"is not a prime number");
+    
 }
+ 
 
